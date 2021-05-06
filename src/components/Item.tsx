846443c2c24aa20b5d22138eeef7540ad50a9399
srinivasan-r23 <srinismart2@gmail.com>
2021-05-06 17:26:46 +0530

    Typescript React --- implemented hooks, useState, useContext -- COMPLETE TODO APP

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import classes from "./Item.module.css";
 
-const Item: React.FC<{ text: string }> = ({ text }) => {
-  return <li>{text}</li>;
+const Item: React.FC<{
+  id: string;
+  text: string;
+  onDeleteHandler: (id: string) => void;
+}> = (props) => {
+  return (
+    <li
+      className={classes.item}
+      onClick={() => props.onDeleteHandler(props.id)}
+    >
+      {props.text}
+    </li>
+  );
 };
 
 export default Item;
